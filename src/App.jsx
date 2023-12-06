8430c53d0c2e86bf39ee45598048fc7cbcf8933e
MauroSuarez1607 <maurosuarez1994@gmail.com>
2023-12-05 22:49:12 -0300

    my first dashboard in react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./assets/css/app.css";
+import { ContentWrapper } from "./components/ContentWrapper";
+import { SideBar } from "./components/SideBar";
+
+function App() {
+  return (
+    <>
+      <div id="wrapper">
+        <SideBar />
+
+        <ContentWrapper />
+      </div>
+    </>
+  );
+}
+
+export default App;
